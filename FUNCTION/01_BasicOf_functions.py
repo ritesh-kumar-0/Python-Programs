67960dc04091c9dc75574566112552c125a95313
Ritesh kumar <sidharthrk7890@gmail.com>
2026-07-21 16:09:37 +0530

    Functions

---
+++ FUNCTION/01_BasicOf_functions.py
@@ -0,0 +1,12 @@
+#Basic of Function 
+#Function Definition 
+
+def avg():
+    a = int(input("Enter number: "))
+    b = int(input("Enter number: "))
+    c = int(input("Enter number: "))
+    
+    average = (a + b + c)/3
+    print(average)
+#Function call 
+avg()
---
+++ FUNCTION/02_Quick_quiz.py
@@ -0,0 +1,11 @@
+#Write a program to greet a user with “Good day” using functions.
+# define a function name greet with one parameter "name"
+
+def greet(name):
+
+    print("Good day,", name )  # here, name is a parameter
+#Taking user input 
+user_name = input("Enter your name: ")
+
+#Call the function 
+greet(user_name)   #user_name is a argument 
