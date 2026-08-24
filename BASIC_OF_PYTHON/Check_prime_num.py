14c68918d26445d3889c9ef9b21057589dd96d3f
Ritesh Kumar <sidharthrk7890@gmail.com>
2026-08-25 01:00:26 +0530

    Updated prime number Program

--- BASIC_OF_PYTHON/Check_prime_num.py
+++ BASIC_OF_PYTHON/Check_prime_num.py
@@ -1,7 +1,19 @@
-num = int(input("Enter Number: "))
-if num % num == 1 or num != 0 :
-    print ("Its prime ")
+
+import math
+
+num = int(input("Enter a number: "))
+
+if num < 2:
+    print(f"{num} is not a prime number")
 else:
-    print("Odd")
+    is_prime = True
 
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
 
+    if is_prime:
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
